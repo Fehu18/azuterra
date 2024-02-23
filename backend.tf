656161bd28cc602ab4a0c4f9be23d45aee7a030d
Fehu18 <145318881+Fehu18@users.noreply.github.com>
2024-02-23 16:42:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -4,6 +4,6 @@ terraform {
     storage_account_name = "backendstore1206"
     container_name       = "storecont"
     key                  = "terraform.tfstate"
-    access_key           + "sp=r&st=2024-02-23T10:59:39Z&se=2024-02-23T18:59:39Z&spr=https&sv=2022-11-02&sr=c&sig=Sz6kyc4vhjizlq3H0J%2FwcerdC05U9C%2FIRepYzZlJFtw%3D"
+    access_key           = "sp=r&st=2024-02-23T10:59:39Z&se=2024-02-23T18:59:39Z&spr=https&sv=2022-11-02&sr=c&sig=Sz6kyc4vhjizlq3H0J%2FwcerdC05U9C%2FIRepYzZlJFtw%3D"
   }
 }
